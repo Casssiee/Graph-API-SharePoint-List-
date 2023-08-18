7a3157e645310f4e7d464cd880fb48d004ed355c
Cassie <96107709+Casssiee@users.noreply.github.com>
2023-08-18 10:39:50 +1000

    Update Checking_list.py

--- Checking_list.py
+++ Checking_list.py
@@ -7,13 +7,13 @@ load_dotenv()
 sharepoint_connection = sharepoint.MicrosoftGraph(client_id=os.getenv('app_registration_client_id'),
                                         tenant_id=os.getenv('app_registration_tenant_id'),
                                         client_credential=os.getenv('app_registration_client_secret'),
-                                        username='blueprism.bot@sydney.edu.au',
+                                        username='the username',
                                         password=os.getenv('sharepoint_password'),
                                         scope=["Sites.ReadWrite.All"])
 
 #print(sharepoint_connection)
-sharepoint_response = sharepoint_connection.get_sharepoint_list(tenant_name='unisyd',
-                                                                team_id='AIHub',
-                                                                list_id='c0135071-12ff-4186-b9f8-d3939d7c54a4')
+sharepoint_response = sharepoint_connection.get_sharepoint_list(tenant_name='the tenant name',
+                                                                team_id='the team id',
+                                                                list_id='the list id')
                                                 
 print(sharepoint_response.json())
